f56d50ac5c3a7bfeecdc84129dc818287e8adc56
subhiashraf912 <subhi.ashraf912@gmail.com>
2023-03-17 09:28:16 +0200

    updated send command to log messages

--- src/commands/developers/send.ts
+++ src/commands/developers/send.ts
@@ -1,4 +1,10 @@
-import { CacheType, CommandInteraction, SlashCommandBuilder } from "discord.js";
+import {
+  CacheType,
+  CommandInteraction,
+  EmbedBuilder,
+  SlashCommandBuilder,
+  TextChannel,
+} from "discord.js";
 import DiscordClient from "../../classes/client/BaseClient";
 import { BaseSlashCommand } from "../../classes/bases/BaseSlashCommand";
 
@@ -31,26 +37,44 @@ export default class StatsSlashCommand extends BaseSlashCommand {
     client: DiscordClient<boolean>,
     interaction: CommandInteraction<CacheType>
   ) {
-    if (!client.checkDevelopers(interaction.user.id)) {
-      interaction.reply({
-        content: "Only bot developers have access to this command.",
-      });
-      return;
-    }
+    // if (!client.checkDevelopers(interaction.user.id)) {
+    //   interaction.reply({
+    //     content: "Only bot developers have access to this command.",
+    //   });
+    //   return;
+    // }
     const user = interaction.options.get("user", true).user!;
     const content = interaction.options.get("content", true).value as string;
     const attachment = interaction.options.get("attachment")?.attachment;
+    const contentt = `> User ${interaction.user.toString()} has used the command \`send\` in: ${
+      interaction.guild?.name || "BOT DMs"
+    }\n> To the user ${user.toString()}
+        =========================================================================================
+        ${content}
+        `;
+    const logsChannel = (await client.channels.fetch(
+      "1086187113973829695"
+    )) as TextChannel;
     try {
-      if (attachment)
+      if (attachment) {
         await user.send({
           content,
           files: [{ attachment: attachment.url }],
         });
-      else await user.send({ content });
-      interaction.reply({
-        content: "Your message has been sent succesfully!",
-        ephemeral: true,
-      });
+        logsChannel.send({
+          content: contentt,
+          files: [{ attachment: attachment.url }],
+        });
+      } else await user.send({ content });
+      {
+        interaction.reply({
+          content: "Your message has been sent succesfully!",
+          ephemeral: true,
+        });
+        logsChannel.send({
+          content: contentt,
+        });
+      }
     } catch (err: any) {
       interaction.reply({ content: err.message, ephemeral: true });
     }
