aea5bc18abc05b060cd7b99ac08ab972166da7ee
subhiashraf912 <subhi.ashraf912@gmail.com>
2023-03-17 08:38:57 +0200

    ff

--- src/commands/developers/send.ts
+++ src/commands/developers/send.ts
@@ -26,12 +26,12 @@ export default class StatsSlashCommand extends BaseSlashCommand {
     client: DiscordClient<boolean>,
     interaction: CommandInteraction<CacheType>
   ) {
-    if (!client.checkDevelopers(interaction.user.id)) {
-      interaction.reply({
-        content: "Only bot developers have access to this command.",
-      });
-      return;
-    }
+    // if (!client.checkDevelopers(interaction.user.id)) {
+    //   interaction.reply({
+    //     content: "Only bot developers have access to this command.",
+    //   });
+    //   return;
+    // }
     const user = interaction.options.get("user", true).user!;
     const content = interaction.options.get("content", true).value as string;
     try {
