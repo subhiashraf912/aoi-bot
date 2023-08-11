5ffc0227b9581c57200685cf540be084661eb0a2
subhiashraf912 <subhi.ashraf912@gmail.com>
2023-08-11 22:33:32 +0300

    some updates idk them

--- src/bot/commands/info/help.ts
+++ src/bot/commands/info/help.ts
@@ -2,10 +2,15 @@ import {
   CacheType,
   CommandInteraction,
   ApplicationCommandOptionType,
+  EmbedBuilder,
+  ActionRowBuilder,
+  StringSelectMenuBuilder,
+  Colors,
+  ButtonBuilder,
+  ButtonStyle,
 } from "discord.js";
 import DiscordClient from "../../classes/client/BaseClient";
 import { BaseSlashCommand } from "../../classes/bases/BaseSlashCommand";
-
 export default class HelpSlashCommand extends BaseSlashCommand {
   constructor() {
     super({
@@ -13,51 +18,87 @@ export default class HelpSlashCommand extends BaseSlashCommand {
       description: "Shows all commands and their descriptions",
     });
   }
-
   async run(
-    client: DiscordClient<boolean>,
+    client: DiscordClient<true>,
     interaction: CommandInteraction<CacheType>
   ) {
-    const commands = await client.application?.commands.fetch();
+    const embed = new EmbedBuilder()
+      .setTitle("Welcome!")
+      .setDescription(
+        "Thank you for using me. You can navigate through the commands using the buttons and menus below."
+      )
+      .setColor(Colors.Blue)
+      .setThumbnail(
+        "https://i.pinimg.com/originals/23/d7/16/23d71629f0ae9f5e26a710e66e792889.gif"
+      )
+      .setTimestamp()
+      .setImage(
+        "https://cdn.discordapp.com/attachments/1128236224193249360/1128236243604471828/beso_1.gif"
+      )
+      .setAuthor({
+        name: client.user.username,
+        iconURL: client.user.displayAvatarURL(),
+      })
+      .setFooter({ text: "0" }); // We start at page 0
 
-    if (!commands) {
-      {
-        interaction.reply({
-          content: "No commands found",
-          ephemeral: true,
-        });
-      }
-      return;
-    }
+    const row1 = new ActionRowBuilder<StringSelectMenuBuilder>().addComponents(
+      new StringSelectMenuBuilder()
+        .setCustomId("help-menu")
+        .setPlaceholder("Select a category...")
+        .addOptions([
+          {
+            label: "Slash Commands",
+            description: "List all slash commands",
+            value: "slash",
+          },
+          {
+            label: "Sub Slash Commands",
+            description: "List all sub slash commands",
+            value: "subslash",
+          },
+        ])
+    );
 
-    let reply = "";
+    const row2 = new ActionRowBuilder<ButtonBuilder>().addComponents(
+      new ButtonBuilder()
+        .setCustomId("help-back")
+        .setLabel("Back")
+        .setStyle(ButtonStyle.Danger)
+        .setEmoji("🔙")
+        .setDisabled(true), // initially disabled
+      new ButtonBuilder()
+        .setLabel("Vote For Me!")
+        .setStyle(ButtonStyle.Link)
+        .setEmoji("🗳️")
+        .setURL(`https://top.gg/bot/${client.user.id}/vote`),
+      new ButtonBuilder()
+        .setLabel("Join Support Server")
+        .setStyle(ButtonStyle.Link)
+        .setEmoji("👥")
+        .setURL("https://discord.gg/TmtwMyCtwh")
+    );
 
-    commands.forEach((command) => {
-      console.log(
-        `Command: ${command.name} | Description: ${command.description}`
-      );
-      reply += `**/${command.name}**: ${command.description}\n`;
+    const row3 = new ActionRowBuilder<ButtonBuilder>().addComponents(
+      new ButtonBuilder()
+        .setLabel("Read the Docs")
+        .setStyle(ButtonStyle.Link)
+        .setEmoji("📖")
+        .setURL("https://google.com"),
+      new ButtonBuilder()
+        .setLabel("Go to dashboard")
+        .setStyle(ButtonStyle.Link)
+        .setEmoji("📈")
+        .setURL("https://google.com"),
+      new ButtonBuilder()
+        .setCustomId("help-forward")
+        .setLabel("Forward")
+        .setEmoji("🔜")
+        .setStyle(ButtonStyle.Success)
+    );
 
-      if (command.options) {
-        command.options.forEach((option) => {
-          switch (option.type) {
-            case ApplicationCommandOptionType.Subcommand:
-            case ApplicationCommandOptionType.SubcommandGroup:
-              reply += `\t**${option.name}**: ${option.description}\n`;
-              break;
-            default:
-              reply += `\t**${option.name}** (type: ${option.type}): ${option.description}\n`;
-              break;
-          }
-        });
-      }
-
-      reply += "\n";
-    });
-
-    interaction.reply({
-      content: reply,
-      ephemeral: true, // Change to false if you want the reply to be public
+    await interaction.reply({
+      embeds: [embed],
+      components: [row1, row2, row3],
     });
   }
 }
--- src/bot/events/interactionCreate.ts
+++ src/bot/events/interactionCreate.ts
@@ -1,5 +1,11 @@
 import BaseEvent from "../classes/bases/BaseEvent";
-import { Interaction } from "discord.js";
+import {
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  EmbedBuilder,
+  Interaction,
+} from "discord.js";
 import DiscordClient from "../classes/client/BaseClient";
 export default class MessageEvent extends BaseEvent {
   constructor() {
@@ -37,6 +43,66 @@ export default class MessageEvent extends BaseEvent {
       } else {
         interaction.reply({ content: "This command has no run method." });
       }
+    }
+
+    if (interaction.isButton()) {
+      if (!["help-back", "help-forward"].includes(interaction.customId)) return;
+
+      const isBack = interaction.customId === "help-back";
+      let pageIndex = parseInt(interaction.message.embeds[0].footer?.text!); // Assume you store the page index in the footer
+
+      const combinedCommands = [
+        ...client.slashCommands.map((command) => command.getSlashCommandJSON()),
+        ...client.subcommandsBuilders.map((command) => command.toJSON()),
+      ];
+
+      const totalPages = Math.ceil(combinedCommands.length / 5);
+
+      if (isBack) pageIndex--;
+      else pageIndex++;
+
+      // Ensure the page index is in the correct range
+      if (pageIndex < 0) pageIndex = 0;
+      if (pageIndex >= totalPages) pageIndex = totalPages - 1;
+
+      const embed = new EmbedBuilder()
+        .setTitle(`Page ${pageIndex + 1} of ${totalPages}`)
+        .setFooter({ text: pageIndex.toString() });
+
+      for (
+        let i = pageIndex * 5;
+        i < (pageIndex + 1) * 5 && i < combinedCommands.length;
+        i++
+      ) {
+        const command = combinedCommands[i];
+        embed.addFields([{ name: command.name, value: command.description }]);
+      }
+
+      const backDisabled = pageIndex == 0;
+      const forwardDisabled = pageIndex == totalPages - 1;
+      const row2 = new ActionRowBuilder<ButtonBuilder>().addComponents(
+        new ButtonBuilder()
+          .setCustomId("help-back")
+          .setLabel("Back")
+          .setStyle(ButtonStyle.Danger)
+          .setEmoji("🔙")
+          .setDisabled(backDisabled),
+        new ButtonBuilder()
+          .setCustomId("help-forward")
+          .setLabel("Forward")
+          .setStyle(ButtonStyle.Success)
+          .setEmoji("🔜")
+          .setDisabled(forwardDisabled)
+      );
+
+      await interaction.update({
+        embeds: [embed],
+        components: [
+          interaction.message.components[0],
+          interaction.message.components[1],
+          row2,
+        ],
+      });
     }
   }
 }
